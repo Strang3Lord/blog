73b4a789f68c63d010750851a380436c0a4903a6
Starng3Lord <piyushmanwar74@gmail.com>
2022-10-19 17:00:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './pages/Header'
+import { Container } from 'react-bootstrap'
+import BlogPage from './pages/BlogPage'
+import Home from './pages/Home'
+import CreateBlog from './pages/CreateBlog'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <main className='main'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/create' element={<CreateBlog />} />
+            <Route path='/:blogId' element={<BlogPage />} />
+            <Route path='/search/:keyword' element={<Home />} />
+          </Routes>
+        </Container>
+      </main>
+    </Router>
+  )
 }
 
-export default App;
+export default App
